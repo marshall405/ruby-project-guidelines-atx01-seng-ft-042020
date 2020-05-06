397fd5d4fb605ba1cfca43facb3eb6ed6403fd38
marshall405 <marshall.slemp@gmail.com>
2020-05-06 10:51:36 -0500

    use active record associations, clean up interface class

--- lib/user.rb
+++ lib/user.rb
@@ -6,47 +6,22 @@ class User < ActiveRecord::Base
 
     @@user_repos = []
 
-    def get_user_repo_ids
-        UserRepo.all.reduce([]) do |memo, user_repo|
-            if user_repo.user_id == self.id 
-                memo << user_repo.repo_id 
-            end
-            memo
-        end
-    end
-
-    def get_user_repos 
-        repos = get_user_repo_ids
-        @@user_repos = Repo.all.select do |repo|
-            repos.include?(repo.id)
-        end
-        display_repos
-    end
-
-    def display_repos
-        @@user_repos.each do |repo|
-            Repo.new(
-                name: repo["name"],
-                description: repo["description"],
-                url: repo["url"],
-                private: repo["private"],
-                owner_id: repo["owner_id"],
-                forks: repo["forks"],
-                stars: repo["watchers_count"]
-            ).display(repo.id)
+    def self.display_repos_by_user(user)
+        user = User.find(user.id)
+        user.repos.each do |repo|
+            repo.display
         end
     end
 
     def delete_repo(repo_id)
         row = UserRepo.where(repo_id: repo_id, user_id: self.id)
         UserRepo.delete(row)
-        puts "repo deleted"
+        "Repo ID: #{repo_id} has been deleted."
     end
 
-    def self.update_username(username, new_name)
-        user = User.find_by(username: username)
-        user.update(username: new_name)
-        user.username
+    def update_username(new_name)
+        User.find(self.id).update(username: new_name)
+        "Username has been changed to #{new_name}"
     end
 
 
--- lib/user_repo.rb
+++ lib/user_repo.rb
@@ -1,13 +1,10 @@
 class UserRepo < ActiveRecord::Base
-
+    belongs_to :user
+    belongs_to :repo 
     
     def self.create(user, repo)
         repo.save 
         super(user_id: user.id, repo_id: repo.id)
     end
-
-    
-
-
 
 end
